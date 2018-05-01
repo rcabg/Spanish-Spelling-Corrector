747f3fb1f0cb41de067cd48e053de797f8158272
Rafael Caballero <rafacg93@gmail.com>
2018-05-01 16:43:07 +0200

    Adding new class to load words and freqs from RAE
    http://corpus.rae.es/lfrecuencias.html

---
+++ SpanishWordFreq.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May  1 16:14:49 2018
+
+@author: rcabg
+"""
+
+class SpanishWordsFreq:
+    def __init__(self, filePath):
+        self.file = open(filePath, "r", encoding = 'utf-8')
+        self.words = {}
+        self.totalFreq = 0
+        self.parseFile()
+        
+    def parseFile(self):
+        next(self.file)
+        for line in self.file:
+            index = line.find(".")
+            try:
+                editedLine = line[index+2:]
+                editedLine = editedLine.replace(',', '')
+                lineSplit = editedLine.split()
+                self.words[lineSplit[0]] = int(lineSplit[1])
+                self.totalFreq += int(lineSplit[1])
+            except Exception:
+                print("It seems there is a problem with the parsed line. Check it out:")
+                print(editedLine)
+                print(lineSplit)
